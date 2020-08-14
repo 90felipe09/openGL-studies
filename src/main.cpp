38e1ddb7da42c3b6fb06e774b04d8f48efb4e92e
90felipe09 <felhipeshiraishi@gmail.com>
2020-08-14 13:53:52 -0300

    Setup: build glew, add it lib and render triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,50 @@
+#define GLEW_NO_GLU
+#define GLEW_STATIC
+#define GLFW_INCLUDE_NONE
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
 #include <thread>
 
+#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 600
+
 int main(){
-    glfwInit();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    glfwTerminate();
+    GLFWwindow* window;
+
+    if(!glfwInit()){
+        return -1;
+    }
+
+    
+    window = glfwCreateWindow(WINDOW_HEIGHT, WINDOW_WIDTH, "Game Engine", NULL, NULL);
+    if (!window){
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK){
+        std::cout << "Error in glewInit()" << std::endl;
+    }
+
+    unsigned int a;
+    glGenBuffers(1, &a);
+
+    while(!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
 }
